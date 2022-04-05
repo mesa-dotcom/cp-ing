8d1f59999440c37943a9fdc8a8e49188597781ac
mesa-dotcom <ctithmesa@gmail.com>
2022-04-05 11:08:00 +0700

    feat: folder structure

---
+++ src/app/_shared/directives/git-ignore.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[compGitIgnore]'
+})
+export class GitIgnoreDirective {
+
+  constructor() { }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_shared/components/footer/footer.component';
+import { HeaderComponent } from './_shared/components/header/header.component';
+import { GitIgnoreDirective } from './_shared/directives/git-ignore.directive';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    GitIgnoreDirective,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
