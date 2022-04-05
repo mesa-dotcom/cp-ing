2b37b84e1895c366a052aefd62d3a74163a74b52
mesa-dotcom <ctithmesa@gmail.com>
2022-04-05 11:41:02 +0700

    doc: add prime-ng flex icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './_shared/components/footer/footer.component';
 import { HeaderComponent } from './_shared/components/header/header.component';
 import { GitIgnoreDirective } from './_shared/directives/git-ignore.directive';
 import { MainScreenComponent } from './main-screen/main-screen.component';
+
+import PRIMENG_MODULE from './modules/prime-ng';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { MainScreenComponent } from './main-screen/main-screen.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...PRIMENG_MODULE
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/prime-ng.ts
@@ -0,0 +1,9 @@
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+
+const PRIMENG_MODULE = [
+    CardModule,
+    InputTextModule
+]
+
+export default PRIMENG_MODULE;
