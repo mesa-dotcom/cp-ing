284944707f190f158b9133f770aa3758792bcdd2
mesa-dotcom <ctithmesa@gmail.com>
2022-04-11 16:12:07 +0700

    feat: ping by time and continuously

--- src/app/_shared/services/ping.service.ts
+++ src/app/_shared/services/ping.service.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { concatMap, interval, map, Observable, repeat } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PingService {
+  constructor(private _http: HttpClient) {}
 
-  constructor(private _http: HttpClient) { }
+  formatOutput(output: string): string {
+    return output.split('Ping statistics')[0];
+  }
 
   ping(ip: string, times: number = 4): Observable<any> {
-    return this._http.post('api/ping', { ip });
+    return this._http
+      .post('api/ping', { ip })
+      .pipe(map((x: any) => this.formatOutput(x.output)), repeat(times));
+  }
+
+  pingContinuously(ip: string): Observable<any> {
+    return interval(1000).pipe(concatMap(() => this.ping(ip, 1)));
   }
 }
--- src/app/play-ground/play-ground.component.ts
+++ src/app/play-ground/play-ground.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  interval,
-  mergeMap,
-  Subscription,
-} from 'rxjs';
+import { interval, mergeMap, Subscription, switchMap } from 'rxjs';
 import { PingService } from '../_shared/services/ping.service';
 
 @Component({
@@ -14,39 +10,45 @@ import { PingService } from '../_shared/services/ping.service';
 export class PlayGroundComponent implements OnInit {
   public isPinging: boolean = false;
   public isContinuous: boolean = false;
-  public ipTest: string = '';
-  public resultSubscription: Subscription = new Subscription()
-  public results: string[] = []
+  public ipTest: string = '127.0.0.1';
+  public resultSubscription: Subscription = new Subscription();
+  public results: string[] = [];
 
-  constructor(private _pingService: PingService) {
-  }
+  constructor(private _pingService: PingService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   start() {
-    if (this.ipTest !== '') {
-      this.isPinging = true
-      this.pingContinuously ? this.pingContinuously() : void 0
-    }
+    if (this.ipTest === '') return;
+    this.isPinging = true;
+    this.isContinuous ? this.pingContinuously() : this.pingNormally();
+  }
+
+  pingNormally() {
+    this.resultSubscription = this._pingService.ping(this.ipTest).subscribe({
+      next: (res: string) => {
+        this.results.push(res);
+      },
+      complete: () => {
+        this.isPinging = false;
+      },
+    });
   }
 
   pingContinuously() {
-    this.resultSubscription = interval(1000).pipe(
-      mergeMap(() => this._pingService.ping(this.ipTest)),
-    ).subscribe(
-      (result) => {
-        this.results.push(result)
-      }
-    )
+    this.resultSubscription = this._pingService
+      .pingContinuously(this.ipTest)
+      .subscribe((x: string) => {
+        this.results.push(x);
+      });
   }
 
   stop() {
-    this.isPinging = false
-    this.resultSubscription.unsubscribe()
+    this.isPinging = false;
+    this.resultSubscription.unsubscribe();
   }
 
   clear() {
-    this.results = []
+    this.results = [];
   }
 }
