f0560f438c73dde887dbf638bc29b728548fa0bd
mesa-dotcom <ctithmesa@gmail.com>
2022-04-26 14:14:02 +0700

    feat: testing

--- src/app/_shared/components/input-form/input-form.component.ts
+++ src/app/_shared/components/input-form/input-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { DeviceABBR, LimitedNumberDevice } from '../../constants';
+import { DeviceABBR, LimitedNumberDevice, UniqueDevices } from '../../constants';
 import { DeviceType } from '../../enums';
 
 @Component({
@@ -9,19 +9,21 @@ import { DeviceType } from '../../enums';
   styleUrls: ['./input-form.component.css'],
 })
 export class InputFormComponent implements OnInit, OnChanges {
+  public styleClass: { [key: string]: string } = {
+    wrapper: 'xl:col-3 lg:col-4 md:col-6 grid align-items-center',
+    label: 'xl:col-3 lg:col-4 md:col-6 align-self-center',
+  };
   deviceAbbr = DeviceABBR;
+  uniqueDevices = UniqueDevices as string[]
   @Input() formGroup: FormGroup = new FormGroup({});
   @Input() deviceName: DeviceType | string = DeviceType.POS;
-  tvalue = [1,2];
   options: { name: string; value: number }[] = [];
   constructor() {}
 
   ngOnChanges(): void {
-    this.tvalue = this.formGroup.controls[this.deviceName].value;
   }
 
   ngOnInit(): void {
-    this.tvalue = this.formGroup.controls[this.deviceName].value;
     console.log(this.formGroup.controls[this.deviceName].value);
     for (let index = 0; index < LimitedNumberDevice[this.deviceName]; index++) {
       this.options.push({
